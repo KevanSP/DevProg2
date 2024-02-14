fb49785c0499184ef9f38141be34dd75870a5696
KevanSP <kevan.22@ichat.sp.edu.sg>
2024-02-14 22:43:08 +0800

    add and fix pytest

---
+++ project code/Test_main.py
@@ -0,0 +1,59 @@
+from unittest.mock import patch
+
+import main as mf
+
+def test_temperatureThread():
+    result = []
+    test_arr = [[90,10], [50,200], [50,50]]
+    pass_arr = [True,True,False]
+
+    for item in test_arr:
+        result = result.append(mf.temperatureThread(True, item))
+
+    assert (result == pass_arr)
+
+def test_slideSwitchThread():
+    result = []
+    test_input = [True,False]
+    pass_arr = [True,False]
+    for item in test_input:
+        with patch('button.read_slide_switch') as test_input:
+
+            test_input.return_value = item
+
+            result = result.append(mf.slideSwitchThread())
+
+    assert (result == pass_arr)
+
+def test_lightingThread():
+    result = []
+    test_input = [800, 600]
+    pass_arr = [True, False]
+    for item in test_input:
+        with patch('adc.get_adc_value') as test_input:
+            test_input.return_value = item
+
+            result = result.append(mf.lightingThread())
+
+    assert (result == pass_arr)
+
+def test_motorThread():
+    result = []
+    test_input = 2
+    pass_arr = [True, 0]
+    with patch('buzz.turn_on') as buzz_on, \
+    patch('valve.set_motor_speed') as motor, \
+    patch('turn_on_led') as led_on, \
+    patch('turn_off_led') as led_off, \
+    patch('buzz.turn_off') as buzz_off:
+        buzz_on.return_value = True
+        buzz_off.return_value = True
+        led_on.return_value = True
+        led_off.return_value = True
+        motor.return_value = True
+
+        result = mf.motorThread(True,test_input)
+
+    assert (result == pass_arr)
+
+
--- project code/main.py
+++ project code/main.py
@@ -1,17 +1,18 @@
-from hal import hal_led as led
-from hal import hal_buzzer as buzz
-from hal import hal_input_switch as button
-from hal import hal_temp_humidity_sensor as temp
-from hal import hal_dc_motor as valve
 from threading import Thread
-from hal import hal_ir_sensor as ir
 from time import sleep
-from hal import  hal_adc as adc
+
+from hal import hal_adc as adc
+from hal import hal_buzzer as buzz
+from hal import hal_dc_motor as valve
+from hal import hal_input_switch as button
+from hal import hal_led as led
+from hal import hal_temp_humidity_sensor as temp
 
 Output = 0
-count =0
+count = 0
 
 Run_Multithread = True  # Define Run_Multithread
+
 
 def initialize_hardware():
     led.init()
@@ -23,39 +24,48 @@ def initialize_hardware():
     valve.set_motor_speed(0)
     turn_off_led()
     buzz.turn_off()
-    
 
-def thread1():
+
+def temperatureThread(test_mode, temp_humi):
     global Output, Run_Multithread
     while Run_Multithread:
-        temperature, humidity = temp.read_temp_humidity()
-        print(temperature,humidity)
+        if not test_mode:
+            temperature, humidity = temp.read_temp_humidity()
+        else:
+            temperature, humidity = temp_humi
+
+        # print(temperature, humidity)
         if temperature > 80 or humidity > 100:
             Output = True
-            
+    return Output
 
-def thread2():
+
+def slideSwitchThread():
     global Output, Run_Multithread
     while Run_Multithread:
-        #print(Output)
-        p = button.read_slide_switch()
-        if p == True:
-            Output = True
-def thread4():
+        # print(Output)
+        Output = button.read_slide_switch()
+    return Output
+
+
+def lightingThread():
     global Output, Run_Multithread
     while Run_Multithread:
         threshold_lighting = 700
         lighting = adc.get_adc_value(0)
-        #print ("Lighting value", lighting)
+        # print ("Lighting value", lighting)
         if lighting > threshold_lighting:
             Output = True
-            
-    
-           
-def thread3():
+    return Output
+
+
+def motorThread(test_mode, test_count):
     global Output, Run_Multithread, count
+    if test_mode:
+        Output = True
+        count = test_count
     while Run_Multithread:
-        while Output == True:
+        while Output:
             if count <= 10:
                 buzz.turn_on()
                 valve.set_motor_speed(60)
@@ -63,37 +73,36 @@ def thread3():
                 sleep(0.5)
                 turn_off_led()
                 sleep(0.5)
-                count+=1
-                print(count)
+                count += 1
+                # print(count)
             else:
                 Output = False
                 count = 0
                 buzz.turn_off()
                 valve.set_motor_speed(0)
-                print(Output)
-      
-    
+                # print(Output)
+    return Output, count
+
+
 def turn_off_led():
     led.set_output(1, 0)
+
 
 def turn_on_led():
     led.set_output(1, 1)
 
+
 def main():
-    t1 = Thread(target=thread1)
+    t1 = Thread(target=temperatureThread)
     t1.start()
-    t2 = Thread(target=thread2)
+    t2 = Thread(target=slideSwitchThread)
     t2.start()
-    t3 = Thread(target=thread3)
+    t3 = Thread(target=motorThread)
     t3.start()
-    t4 = Thread(target=thread4)
+    t4 = Thread(target=lightingThread)
     t4.start()
 
-    
-        # Note: count is not defined or used in the original code, so I'm assuming it's not needed.
 
 if __name__ == "__main__":
     initialize_hardware()
     main()
-
-
